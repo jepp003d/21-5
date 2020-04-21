39b8bd4a061cdced94462901b4a8d9a5978e5f7d
andr60c0 <andr60c0@stud.kea.dk>
2020-04-21 13:53:44 +0200

    backgroundchange.js

---
+++ backgroundchange.js
@@ -0,0 +1,43 @@
+var test = document.getElementById("test");
+let curSection;
+let prevSection;
+
+document.onscroll = function () {
+
+    scrollTop = document.documentElement.scrollTop;
+    test.innerHTML = scrollTop;
+
+    allSections = document.getElementsByTagName('section');
+
+    for (i = 0; i < allSections.length; i++) {
+        prevSection = curSection;
+        curSection = allSections[i];
+
+        console.log("prev", prevSection);
+
+        console.log("curSection", curSection);
+
+
+
+        //  Ændrer baggrundsfarve når man skroller forbi 2/3 del af den forrige sektion
+        heightBefore = 0;
+        if (i > 0) {
+            heightBefore = allSections[i - 1].offsetHeight / 3;
+        }
+
+        if (scrollTop > curSection.offsetTop - heightBefore) {
+            curColor = curSection.getAttribute("data-color");
+            prevColor = "#C95D43";
+            curSection.style.background = curColor;
+            prevSection.style.background = prevColor;
+
+
+
+        }
+
+
+
+
+    };
+
+}
